9540683f9383cdf7784642c5259f295548c63fd3
Alex Kraken Hawthorne <19832622+AlexKraken@users.noreply.github.com>
2023-08-07 15:32:12 -0500

    Move 'import log' function logic to init

--- sku_logger.py
+++ sku_logger.py
@@ -1,44 +1,41 @@
-class SKU_Logger:
-    """ Create a SKU logger.
+class SKULogger:
+    """Create a SKU logger.
 
-    This SKU logger can create/open a log file, load any previous SKUs 
-    into the program, check if a SKU is already logged, and write new 
-    SKUs into the log file. 
+    This SKU logger can create/open a log file, load any previous SKUs
+    into the program, check if a SKU is already logged, and write new
+    SKUs into the log file.
 
     """
-        
+
     def __init__(self, log_file: str):
         # Use a set to enforce uniqueness
-        self.item_log = set()
         self.log_file = log_file
-        
+        self.sku_log = set()
+
         # Import the log file
         try:
-            self.import_log(self.log_file, self.item_log)
+            with open(log_file, "r") as log_reader:
+                for barcode in log_reader:
+                    self.sku_log.add(barcode.strip())
         except FileNotFoundError:
             create_new_file = input("File not found, create new file? [y/n]: ")
-            if(create_new_file.lower() == 'y'):
-                open(self.log_file, 'a').write("SKUs\n")
+            if create_new_file.lower() == "y":
+                open(self.log_file, "a").write("SKUs\n")
 
     def add(self) -> None:
-        with open(self.log_file, 'a') as log_writer:
+        with open(self.log_file, "a") as log_writer:
             barcode = input("Scan barcode\n").strip()
             log_writer.write(barcode + "\n")
-            self.item_log.add(barcode)
-    
+            self.sku_log.add(barcode)
+
     def check(self) -> None:
         looping = True
-        while(looping):
+        while looping:
             barcode = input("Scan barcode (type in 'exit' when done):\n").strip()
             if barcode.lower() == "exit":
                 looping = False
                 continue
-            if barcode in self.item_log:
+            if barcode in self.sku_log:
                 print("PREVIOUSLY FIXED\n")
             else:
                 print("NOT FOUND\n")
-
-    def import_log(self, log_file: str, item_log) -> None:
-        with open(log_file, 'r') as log_reader:
-            for barcode in log_reader:
-                item_log.add(barcode.strip())
