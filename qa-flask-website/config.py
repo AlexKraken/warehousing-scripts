27433c3c19fc488715e30a9eea1bd61493c274e7
Alex Kraken Hawthorne <19832622+AlexKraken@users.noreply.github.com>
2023-12-08 07:55:58 -0600

    Add comments

--- qa-flask-website/config.py
+++ qa-flask-website/config.py
@@ -2,7 +2,19 @@ import os
 
 
 class Config:
-    DB_NAME = "barcodes.db"
+    """Stores the configuration variables for the application.
+    Import them using
+        `app.config.from_object(Config)`
+    """
+
+    # Secret key for cryptographic operations
     SECRET_KEY = os.environ.get("SECRET_KEY", "default_secret_key_0123456789ABCDEF")
+
+    # SameSite attribute and secure flag for session cookies
+    SESSION_COOKIE_SAMESITE = "None"
+    SESSION_COOKIE_SECURE = True
+
+    # Database configuration for Flask-SQLAlchemy
+    DB_NAME = "barcodes.db"
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{DB_NAME}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
